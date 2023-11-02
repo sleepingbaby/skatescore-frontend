4d95eebf344ed5b3ad75c0c17d4aaf109020b0fe
zachj95 <zachariejones@gmail.com>
2023-11-02 15:26:38 -0700

    Add theme colors & Poppins font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
 import DaisyUIPlugin from "daisyui";
 
-/ @type {import('tailwindcss').Config} */;
+/ @type {import("tailwindcss").Config} */;
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
   plugins: [DaisyUIPlugin],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      text: "#040506",
+      background: "#F9FAFB",
+      primary: "#98A9B3",
+      secondary: "#C7D0D6",
+      accent: "#596D78",
+    },
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+    },
+  },
 };
