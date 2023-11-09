cfd78b237dff3a2ebca7f39bb3b1556bd8dcb5b2
zachj95 <zachariejones@gmail.com>
2023-11-09 15:33:25 -0800

    Remove sample stories, add TextInput component, add TextInput to Storybook, update some Tailwind theme styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ import DaisyUIPlugin from "daisyui";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   plugins: [DaisyUIPlugin],
+  themes: ["light"],
   theme: {
     screens: {
       sm: "480px",
@@ -19,6 +20,15 @@ export default {
         primary: "#98A9B3",
         secondary: "#C7D0D6",
         accent: "#596D78",
+      },
+      fontSize: {
+        small: "0.875rem",
+        p: "1rem",
+        h5: "1rem",
+        h4: "1.25rem",
+        h3: "1.5rem",
+        h2: "1.75rem",
+        h1: "2.5rem",
       },
     },
     fontFamily: {
