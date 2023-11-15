1fd873ad6c48db605a84411a997f682c7ff69895
Kelvin Lau <crosby2987@gmail.com>
2023-11-14 21:54:07 -0800

    Created custom table component and cleaned up logic. Need to make table components

--- src/components/CustomTable.tsx
+++ src/components/CustomTable.tsx
@@ -15,125 +15,66 @@ import { SupabaseContext } from "../providers/SupabaseProvider";
 </CustomTable>
 */
 
-const CustomTable = () => {
-  const supabase = useContext(SupabaseContext);
-  const [tableData, setTableData] = useState({});
+// const supabase = useContext(SupabaseContext);
+//   const [tableData, setTableData] = useState({});
 
-  useEffect(() => {
-    const fetchData = async () => {
-      if (supabase) {
-        try {
-          const { data, error } = await supabase
-            .from("game")
-            .select("*")
-            .limit(16);
-          if (error) {
-            console.error("Error fetching data:", error);
-          } else {
-            setTableData(data);
-          }
-        } catch (error) {
-          console.error("An error occurred:", error);
-        }
-      }
-    };
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       if (supabase) {
+//         try {
+//           const { data, error } = await supabase
+//             .from("game")
+//             .select("*")
+//             .limit(16);
+//           if (error) {
+//             console.error("Error fetching data:", error);
+//           } else {
+//             setTableData(data);
+//           }
+//         } catch (error) {
+//           console.error("An error occurred:", error);
+//         }
+//       }
+//     };
 
-    fetchData();
-  }, [supabase]);
+//     fetchData();
+//   }, [supabase]);
+
+//   console.log(tableData);
+const CustomTable = ({ tableData }) => {
   return (
     <div className="flex-1 border border-slate-500 rounded-t-lg">
       <table className="table-fixed border-spacing-0 border-separate rounded-t-lg w-full">
         <thead className="text-xs text-white uppercase dark:bg-accent">
-          <tr className="h-10">
-            <th className="rounded-tl-md">Date</th>
-            <th>Team(H)</th>
-            <th>Team(A)</th>
-            <th>Score(H)</th>
-            <th>Score(A)</th>
-            <th>Season</th>
-            <th>Division</th>
-            <th className="rounded-tr-md">Rink</th>
-          </tr>
+          {tableData.length > 0 && (
+            <tr className="h-10">
+              {Object.keys(tableData[0]).map((key: string, index: number) => (
+                <th
+                  key={index}
+                  className={`${
+                    index === 0
+                      ? "rounded-tl-md"
+                      : index === Object.keys(tableData[0]).length - 1
+                      ? "rounded-tr-md"
+                      : ""
+                  }`}
+                >
+                  {key}
+                </th>
+              ))}
+            </tr>
+          )}
         </thead>
         <tbody>
-          <tr className="hover:bg-gray-200 h-[48px]">
-            <td className="min-w-[120px] text-center py-2">
-              <small>9/5/22</small>
-            </td>
-            <td className="min-w-[120px] text-center py-2 ">
-              <small>Rogue Squadron</small>
-            </td>
-            <td className="min-w-[120px] text-center py-2">
-              <small>Chupacabra</small>
-            </td>
-            <td className="min-w-[120px] text-center py-2">
-              <small>4</small>
-            </td>
-            <td className="min-w-[120px] text-center py-2">
-              <small>2</small>
-            </td>
-            <td className="min-w-[120px] text-center py-2">
-              <small>Fall 2022</small>
-            </td>
-            <td className="min-w-[120px] text-center py-2">
-              <small>7A</small>
-            </td>
-            <td className="min-w-[120px] text-center py-2">
-              <small>San Jose East</small>
-            </td>
-          </tr>
-          <tr className="hover:bg-gray-200 h-[48px]">
-            <td className="min-w-[120px] text-center py-2">
-              <small>9/5/22</small>
-            </td>
-            <td className="min-w-[120px] text-center py-2 ">
-              <small>Rogue Squadron</small>
-            </td>
-            <td className="min-w-[120px] text-center py-2">
-              <small>Chupacabra</small>
-            </td>
-            <td className="min-w-[120px] text-center py-2">
-              <small>4</small>
-            </td>
-            <td className="min-w-[120px] text-center py-2">
-              <small>2</small>
-            </td>
-            <td className="min-w-[120px] text-center py-2">
-              <small>Fall 2022</small>
-            </td>
-            <td className="min-w-[120px] text-center py-2">
-              <small>7A</small>
-            </td>
-            <td className="min-w-[120px] text-center py-2">
-              <small>San Jose East</small>
-            </td>
-          </tr>
-          <tr className="hover:bg-gray-200 h-[48px]">
-            <td className="min-w-[120px] text-center py-2">
-              <small>9/5/22</small>
-            </td>
-            <td className="min-w-[120px] text-center py-2 ">
-              <small>Rogue Squadron</small>
-            </td>
-            <td className="min-w-[120px] text-center py-2">
-              <small>Chupacabra</small>
-            </td>
-            <td className="min-w-[120px] text-center py-2">
-              <small>4</small>
-            </td>
-            <td className="min-w-[120px] text-center py-2">
-              <small>2</small>
-            </td>
-            <td className="min-w-[120px] text-center py-2">
-              <small>Fall 2022</small>
-            </td>
-            <td className="min-w-[120px] text-center py-2">
-              <small>7A</small>
-            </td>
-            <td className="min-w-[120px] text-center py-2">
-              <small>San Jose East</small>
-            </td>
-          </tr>
+          {tableData.map((row, rowIndex) => (
+            <tr key={rowIndex} className="hover:bg-gray-200 h-[48px]">
+              {Object.values(row).map((cell, cellIndex) => (
+                <td key={cellIndex} className="min-w-[120px] text-center py-2">
+                  <small>{cell}</small>
+                </td>
+              ))}
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
